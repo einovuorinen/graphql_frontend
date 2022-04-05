87ea184d99700514c1c32aa34274a925ee44a28b
Eino Vuorinen <eino.vuorinen@aalto.fi>
2022-04-05 15:11:31 +0300

    8.10 done. adding books woks and books and authors get refreshed

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,15 +1,6 @@
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from './queries'
 
-const ALL_AUTHORS = gql`
-  query {
-    allAuthors  {
-      name
-      born
-      bookCount
-      id
-    }
-  }
-`
 
 const Authors = (props) => {
   const result = useQuery(ALL_AUTHORS)
