a8a41d4a470b18adaa34483710b3f5063f2aaca1
Eino Vuorinen <eino.vuorinen@aalto.fi>
2022-04-05 13:45:53 +0300

    some refactoring done and task8.9. done. allbooks page works now

--- src/App.js
+++ src/App.js
@@ -2,27 +2,9 @@ import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
-import { gql, useQuery } from '@apollo/client'
-
-const ALL_AUTHORS = gql`
-  query {
-    allAuthors  {
-      name
-      born
-      bookCount
-      id
-    }
-  }
-`
 
 const App = () => {
-  const result = useQuery(ALL_AUTHORS)
   const [page, setPage] = useState('authors')
-
-
-  if (result.loading)  {
-    return <div>loading...</div>
-  }
 
   return (
     <div>
@@ -32,7 +14,7 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors show={page === 'authors'} authors={result.data.allAuthors} />
+      <Authors show={page === 'authors'}/>
 
       <Books show={page === 'books'} />
 
--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,8 +1,28 @@
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors  {
+      name
+      born
+      bookCount
+      id
+    }
+  }
+`
+
 const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+  
   if (!props.show) {
     return null
   }
-  const authors = props.authors
+
+  if ( result.loading ) {
+    return <div>loading...</div>
+  }
+
+  const authors = result.data.allAuthors
 
   return (
     <div>
